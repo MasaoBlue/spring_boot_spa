c72ff37646dff1e29ae36256172f1ac269806e5d
MasaoBlue <masaoki62@gmail.com>
2018-07-29 23:47:08 +0900

    ブラウザアクセス時、derbyにアクセスできるようにした

--- src/main/java/com/example/RequestController.java
+++ src/main/java/com/example/RequestController.java
@@ -1,12 +1,34 @@
 package com.example;
+import java.util.List;
+import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class RequestController {
+    @Autowired
+    NamedParameterJdbcTemplate jdbcTemplate;
+    
+    public void run(String... strings) throws Exception {
 
+        String sql = "select id from customer";
+        SqlParameterSource param = new MapSqlParameterSource();
+        List<Map<String, Object>> list = jdbcTemplate.queryForList(sql, param);
+        System.out.println("result =" + list);
+    }
+    
     @RequestMapping(value="/")
     private String hello(){
-        return "Hello world!!";
+        try {
+            run("a", "b");
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return "index";
     }
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+create table customer (
+    id int primary key,
+    name varchar(50),
+    email varchar(50)
+);
