bb06b8e67fc05db18448510c0de6e124b359b67c
MasaoBlue <masaoki62@gmail.com>
2018-08-01 07:24:28 +0900

    RestControllerを使って最初のhtmlを返せるようにした

--- src/main/java/com/example/RequestController.java
+++ src/main/java/com/example/RequestController.java
@@ -2,19 +2,19 @@ package com.example;
 import java.util.List;
 import java.util.Map;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.stereotype.Controller;
 
-@Controller
-public class RequestController {
+@RestController
+public class RequestController{
     @Autowired
     NamedParameterJdbcTemplate jdbcTemplate;
     
     public void run(String... strings) throws Exception {
-
         String sql = "select id from customer";
         SqlParameterSource param = new MapSqlParameterSource();
         List<Map<String, Object>> list = jdbcTemplate.queryForList(sql, param);
@@ -22,13 +22,15 @@ public class RequestController {
     }
     
     @RequestMapping(value="/")
-    private String hello(){
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
         try {
             run("a", "b");
         } catch (Exception e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return "index";
+        return modelAndView;
     }
 }
